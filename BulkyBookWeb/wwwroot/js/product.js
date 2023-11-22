b255c570642f1360e81555decdb695df69d3d47e
Marissa Campa Github <marissag.campa@gmail.com>
2023-11-22 14:05:56 -0500

    added edit button to the product's index page

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -11,7 +11,16 @@ function loadDataTable() {
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "20%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'category.name', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                    <a class="btn btn-danger mx-2"><i class="bi bi-trash3"></i> Delete</a>
+                    </div>`
+                }
+            }
         ]
     });
 };
