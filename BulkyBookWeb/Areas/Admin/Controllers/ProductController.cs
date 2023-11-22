384fab2fa3af3c59df7775faa0ec704fb29d1bca
Marissa Campa <marissag.campa@gmail.com>
2023-11-22 15:08:35 -0500

    1 add datatables api (#2)
    * Added the css and js includes. Added the API GET call with the product list. Populated the data table.
    
    * added gitignore file
    
    * Drop files from .gitignore
    
    * added edit button to the product's index page
    
    * added delete button to the product's index table
    
    * addded SweetAlert confirmation message for delete product

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -88,7 +88,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 					_unitOfWork.Product.Update(productVM.Product);
 				}
 				_unitOfWork.Save();
-				TempData["success"] = "Product created successfully";
+				TempData["success"] = productVM.Product.Id == 0 ? "Product created successfully" : "Product updated successfully";
 				return RedirectToAction("Index");
 			}
 			else
@@ -103,35 +103,37 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 			return View(productVM);
 		}
 
-		// GET
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+			return Json(new { data = objProductList });
+		}
+
 		public IActionResult Delete(int? id)
 		{
-			if (id == null || id == 0)
+			var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+			if (productToBeDeleted == null)
 			{
-				return NotFound();
+				return Json(new { success = false, message = "Error while deleting" });
 			}
-			Product productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
 
-			if (productFromDb == null)
-			{
-				return NotFound();
-			}
-			return View(productFromDb);
-		}
+			var oldImagePath =
+				Path.Combine(_webHostEnvironment.WebRootPath, 
+							productToBeDeleted.ImageUrl.TrimStart('\\'));
 
-		// POST
-		[HttpPost, ActionName("Delete")]
-		public IActionResult DeletePOST(int? id)
-		{
-			Product obj = _unitOfWork.Product.Get(u => u.Id == id);
-			if (obj == null)
+			if (System.IO.File.Exists(oldImagePath))
 			{
-				return NotFound();
+				System.IO.File.Delete(oldImagePath);
 			}
-			_unitOfWork.Product.Remove(obj);
+
+			_unitOfWork.Product.Remove(productToBeDeleted);
 			_unitOfWork.Save();
-			TempData["success"] = "Product deleted successfully";
-			return RedirectToAction("Index");
+
+			return Json(new { success = true, message = "Delete Successful" });
 		}
+
+		#endregion
 	}
 }
