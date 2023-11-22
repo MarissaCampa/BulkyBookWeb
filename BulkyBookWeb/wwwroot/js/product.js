872195a7f3403c84dfa28add4be3ed0005bc9258
Marissa Campa Github <marissag.campa@gmail.com>
2023-11-22 14:58:30 -0500

    addded SweetAlert confirmation message for delete product

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -1,3 +1,4 @@
+var dataTable;
 
 $(document).ready(function () {
     loadDataTable();
@@ -17,10 +18,33 @@ function loadDataTable() {
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
-                    <a href="/admin/product/delete?id=${data}" class="btn btn-danger mx-2"><i class="bi bi-trash3"></i> Delete</a>
+                    <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash3"></i> Delete</a>
                     </div>`
                 }
             }
         ]
     });
 };
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    });
+}
