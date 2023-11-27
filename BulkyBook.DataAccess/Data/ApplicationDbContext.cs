8e4479f221d8c70230cecadeda450808c334608f
Marissa Campa Github <marissag.campa@gmail.com>
2023-11-27 16:41:15 -0600

    Added Details Action Method. Added the Details view. Updated the Details button in the Home Page.

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -24,17 +24,17 @@ namespace BulkyBook.DataAccess.Data
             modelBuilder.Entity<Product>().HasData(
 				new Product
 				{
-				Id = 1,
-				Title = "Fortune of Time",
-				Author = "Billy Spark",
-				Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-				ISBN = "SWD9999001",
-				ListPrice = 99,
-				Price = 90,
-				Price50 = 85,
-				Price100 = 80,
-				CategoryId = 1,
-				ImageUrl = ""
+					Id = 1,
+					Title = "Fortune of Time",
+					Author = "Billy Spark",
+					Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+					ISBN = "SWD9999001",
+					ListPrice = 99,
+					Price = 90,
+					Price50 = 85,
+					Price100 = 80,
+					CategoryId = 1,
+					ImageUrl = ""
 				},
 				new Product
 				{
