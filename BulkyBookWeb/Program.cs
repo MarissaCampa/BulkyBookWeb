91d5b9a7c279d4cd9249a8d2518c16fa5cde16f9
Marissa Campa <marissag.campa@gmail.com>
2023-11-30 14:28:28 -0600

    Add User Identity Authentication (#7)
    * Installed IdentityFrameworkCore, and added a new Identity scafolded item to the web project
    
    * Added _LoginPartial to _Layout to show Register and Login options. Removed the added string connection. Removed the option to require email confirmation for users. Added UserAuthentication and ability to load Razor Pages.
    
    * Added migrations for Identity Tables
    
    * Added ApplicationUser class that extends IdentityUser with properties like Name, StreetAddress, etc. Added the migrations file to add the columns to the user-related tables.
    
    * Register a user as an ApplicationUser instead of an IdentityUser
    
    * Added user roles to the Utility.SD class. Added a RoleManager to the Register page, and it creates the 4 roles on Get (in the table AspNetRoles). Implemented EmailSender to avoid the error. Registered the EmailSender as a service in Program.cs.
    
    * Added Role assigning capabilities when registering as a new user. Added SelectRole drop-down menu in the Register view.
    
    * Added Authorization for the Admin controllers Product and Category for only Admin users. Added builder.Services.ConfigureApplicationCookie for LoginPath, LogoutPath and AccessDeniedPath, so that non-Admin users are redirected correctly.
    
    * Modified Register and Login UI
    
    * Added more fields to the Input model in Register, like Name, StreetAddress, PhoneNumber, etc. These fields are in the ApplicationUser class.
    
    * Updated the Profile UI. Added Display Name annotations to the Register properties StreetAddress, PostalCode, PhoneNumber.

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -2,6 +2,10 @@ using BulkyBook.DataAccess.Data;
 using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BulkyBook.Utility;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +14,16 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
@@ -26,9 +39,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
