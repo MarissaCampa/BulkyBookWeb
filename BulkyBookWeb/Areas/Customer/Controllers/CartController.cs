9850bd36643074167f3a523cc3fea7978d9cdda3
Marissa Campa Github <marissag.campa@gmail.com>
2023-12-05 20:06:27 -0600

    Added Summary action method to CartController. Right now it only returns a simple html Summary View.

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -40,6 +40,11 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
         public IActionResult Plus(int cartId)
         {
             ShoppingCart cartObj = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
