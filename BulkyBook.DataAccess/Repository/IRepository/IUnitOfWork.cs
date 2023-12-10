988fe3a6827cd8e31eeccc401c70e668c0686c54
Marissa Campa Github <marissag.campa@gmail.com>
2023-12-09 20:16:48 -0600

    Added OrderHeader and OrderDetails to Repository and UnitOfWork

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -13,6 +13,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 		ICompanyRepository Company { get; }
 		IShoppingCartRepository ShoppingCart { get; }
 		IApplicationUserRepository ApplicationUser { get; }
+		IOrderHeaderRepository OrderHeader { get; }
+		IOrderDetailRepository OrderDetail { get; }
 
 		void Save();
 	}
