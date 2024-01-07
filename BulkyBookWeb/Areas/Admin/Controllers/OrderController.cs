d3b95e2ffd7d0bf86a8042932d95cdc71da99ed6
Marissa Campa Github <marissag.campa@gmail.com>
2024-01-07 12:26:37 -0500

    Modified OrderController API call GetAll, to receive the filter status and send back only entries with that filter.

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -24,9 +24,28 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
 		#region API CALLS
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+			switch (status)
+			{
+				case "pending":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+					break;
+				case "inprocess":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+					break;
+				case "completed":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+					break;
+				case "approved":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+					break;
+				default:
+					break;
+			}
+
 			return Json(new { data = objOrderHeaders });
 		}
 		#endregion
--- BulkyBookWeb/wwwroot/js/order.js
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -1,12 +1,27 @@
 var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else if (url.includes("completed")) {
+        loadDataTable("completed");
+    }
+    else if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        loadDataTable("all");
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
             { data: 'name', "width": "20%" },
