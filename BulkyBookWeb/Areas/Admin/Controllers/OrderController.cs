e9ea919a6d298c1415d3dcac30ac270663a647b8
Marissa Campa Github <marissag.campa@gmail.com>
2024-01-16 16:13:09 -0500

    Implemented CancelOrder action method, with the option of getting refunded from Stripe where the payment had been approved, and not shipped yet.

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using Stripe.Climate;
 using System.Security.Claims;
 
@@ -93,8 +94,35 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             TempData["success"] = "Order Shipped Successfully";
             return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
         }
-        
-		#region API CALLS
+
+		[HttpPost]
+		[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+		public IActionResult CancelOrder()
+		{
+			var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+
+			if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+			{
+				var options = new Stripe.RefundCreateOptions
+				{
+					Reason = RefundReasons.RequestedByCustomer,
+					PaymentIntent = orderHeader.PaymentIntentId
+				};
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+				_unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            }
+			else
+			{
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+            }
+            _unitOfWork.Save();
+            TempData["success"] = "Order Cancelled Successfully";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+
+        #region API CALLS
         [HttpGet]
 		public IActionResult GetAll(string status)
 		{
