afed6615420fbff18f2355b12b239041a1c5db7f
Marissa Campa Github <marissag.campa@gmail.com>
2024-02-09 09:18:18 -0500

    Added a "CreateUser" option in Content Management for Admin. Now Admin can register a new user. And it doesn't un-sign the admin account.

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -183,6 +183,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
+                HttpContext.Session.Clear();
 			}
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
