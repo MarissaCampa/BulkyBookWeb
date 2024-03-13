be7da1136f3e9f047a6225daedc49caee1ee07d5
Marissa Gisel <marissag.campa@gmail.com>
2024-03-13 09:52:48 -0400

    Added a Role property to the ApplicationUser model, and added a section to retrieve the role names for each user in the GetAll API in the UserController

--- BulkyBookWeb/wwwroot/js/user.js
+++ BulkyBookWeb/wwwroot/js/user.js
@@ -8,20 +8,20 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/user/getall' },
         "columns": [
-            { "data": "name", "width": "15%" },
-            { "data": "email", "width": "15%" },
-            { "data": "phoneNumber", "width": "10%" },
-            { "data": "company.name", "width": "15%" },
-            { "data": "", "width": "8%" },
+            { data: 'name', "width": "15%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'company.name', "width": "15%" },
+            { data: 'role', "width": "15%" },
 
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                     <a href="/admin/user/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
-                    <a onClick=Delete('/admin/user/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash3"></i> Delete</a>
                     </div>`
-                }
+                },
+                "width": "25%"
             }
         ]
     });
