4e7610800db043688995a11581eace1d6c66ff7d
Marissa Gisel <marissag.campa@gmail.com>
2024-04-11 14:20:12 -0400

    Included ProductImages to Product View Model in the upsert get action, to grab the images associated to the product id. Added a foreach loop to the product upsert view, to display each product image in a div. Also, removed some plugins from the tinymce script because they are no longer in the free version.

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -49,7 +49,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 			else
 			{
 				// update
-				productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+				productVM.Product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties:"ProductImages");
 				return View(productVM);
 			}
 		}
