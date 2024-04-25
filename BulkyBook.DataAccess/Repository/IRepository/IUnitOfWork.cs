c30f12722beeaf4522899f7cffc37a5cd4cc6a03
Marissa Gisel <marissag.campa@gmail.com>
2024-04-25 13:35:33 -0400

    Working version. Gotten it up to where from product management you can upload multiple files and display them in product details. Right before adding the delete buttons to the timages in the product details view.

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -15,6 +15,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
 		IApplicationUserRepository ApplicationUser { get; }
 		IOrderHeaderRepository OrderHeader { get; }
 		IOrderDetailRepository OrderDetail { get; }
+		IProductImageRepository ProductImage {  get; } 
 
 		void Save();
 	}
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -18,17 +18,19 @@ namespace BulkyBook.DataAccess.Repository
 		public IApplicationUserRepository ApplicationUser { get; private set; }
 		public IOrderHeaderRepository OrderHeader { get; private set; }
 		public IOrderDetailRepository OrderDetail { get; private set; }
+		public IProductImageRepository ProductImage { get; private set; }
 
 		public UnitOfWork(ApplicationDbContext db)
 		{
 			_db = db;
-			Category = new CategoryRepository(db);
-			Product = new ProductRepository(db);
-			Company = new CompanyRepository(db);
-			ShoppingCart = new ShoppingCartRepository(db);
-			ApplicationUser = new ApplicationUserRepository(db);
-			OrderHeader = new OrderHeaderRepository(db);
-			OrderDetail = new OrderDetailRepository(db);
+			ProductImage = new ProductImageRepository(_db);
+			Category = new CategoryRepository(_db);
+			Product = new ProductRepository(_db);
+			Company = new CompanyRepository(_db);
+			ShoppingCart = new ShoppingCartRepository(_db);
+			ApplicationUser = new ApplicationUserRepository(_db);
+			OrderHeader = new OrderHeaderRepository(_db);
+			OrderDetail = new OrderDetailRepository(_db);
 		}
 
 		public void Save()
