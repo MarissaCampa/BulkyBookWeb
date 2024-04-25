59af24c296832794d7552bebd44b3b83c0515097
Marissa Gisel <marissag.campa@gmail.com>
2024-04-25 15:54:30 -0400

    Display first of ProductImages for each product in the Shopping Cart Index view

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -43,8 +43,11 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 OrderHeader = new()
             };
 
+            IEnumerable<ProductImage> productImages = _unitOfWork.ProductImage.GetAll();
+
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
+                cart.Product.ProductImages = productImages.Where(u => u.ProductId == cart.ProductId).ToList();
                 cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
             }
