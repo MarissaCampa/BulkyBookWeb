2b4d5bddbe2b7341c5e7b9f275d80ca86ea28027
Marissa Gisel <marissag.campa@gmail.com>
2024-04-26 16:45:36 -0400

    Updated UserController to use unitOfWork instead of applicationDbContext directly. Added a RoleManager to UserController. Implemented the Update method in the ApplicationUserRepository.

--- BulkyBook.DataAccess/Repository/ApplicationUserRepository.cs
+++ BulkyBook.DataAccess/Repository/ApplicationUserRepository.cs
@@ -17,5 +17,10 @@ namespace BulkyBook.DataAccess.Repository
         {
             _db = db;
         }
+
+        public void Update(ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Update(applicationUser);
+        }
 	}
 }
